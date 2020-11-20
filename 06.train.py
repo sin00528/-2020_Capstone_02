cb3284fed9764f26f261c870cae9379a16f0d113
sin00528 <sin00528@gmail.com>
2020-11-21 07:54:41 +0900

    update train.py

--- 06.train.py
+++ 06.train.py
@@ -26,7 +26,7 @@ IMG_PATH = './img_seq/train/'
 os.makedirs('./gan_images/', exist_ok=True)
 OUT_PATH = './gan_images/'
 
-EPOCHS = 100
+EPOCHS = 20
 RND_SEED = 42
 BATCH_SIZE = 128
 IMG_HEIGHT = 128
@@ -45,8 +45,14 @@ tf.random.set_seed(RND_SEED)
 # 1. load dataset
 label_path = os.path.join(LABEL_PATH, 'img_label.csv')
 label = pd.read_csv(label_path)
+#label = label[:1280]
 
-train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2, horizontal_flip=True)
+def prep_fn(img):
+    img = img.astype(np.float32) / 255.0
+    img = (img - 0.5) * 2
+    return img
+
+train_datagen = ImageDataGenerator(preprocessing_function=prep_fn, validation_split=0.2, horizontal_flip=True)
 
 training_set = train_datagen.flow_from_dataframe(label,
                                                 x_col = 'file_path',
@@ -55,6 +61,7 @@ training_set = train_datagen.flow_from_dataframe(label,
                                                 batch_size=BATCH_SIZE,
                                                 class_mode="multi_output",
                                                 subset='training')
+
 
 validataion_set = train_datagen.flow_from_dataframe(label,
                                                 x_col = 'file_path',
@@ -166,23 +173,16 @@ def d_loss_fn(real_img, fake_img):
     return total_loss
 
 # 3.2. optimizers
-#img_id_encoder_optimizer = Adam(lr=1e-4, beta_1=0.5, beta_2=0.999)
-#img_exp_encoder_optimizer = Adam(lr=1e-4, beta_1=0.5, beta_2=0.999)
-
 img_id_classifier_optimizer = Adam(lr=1e-4, beta_1=0.5, beta_2=0.999)
 img_exp_classifier_optimizer = Adam(lr=1e-4, beta_1=0.5, beta_2=0.999)
 
 img_decoder_optimizer = Adam(lr=1e-4, beta_1=0.5, beta_2=0.999)
 img_discriminator_optimizer = Adam(lr=1e-4, beta_1=0.5, beta_2=0.999)
 
-
-
 # 4. save ckpt
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(#img_exp_encoder_optimizer=img_exp_encoder_optimizer,
-                                #img_id_encoder_optimizer=img_id_encoder_optimizer,
-                                img_decoder_optimizer=img_decoder_optimizer,
+checkpoint = tf.train.Checkpoint(img_decoder_optimizer=img_decoder_optimizer,
                                 img_discriminator_optimizer=img_discriminator_optimizer,
                                 img_id_classifier_optimizer=img_id_classifier_optimizer,
                                 img_exp_classifier_optimizer=img_exp_classifier_optimizer,
@@ -194,20 +194,20 @@ checkpoint = tf.train.Checkpoint(#img_exp_encoder_optimizer=img_exp_encoder_opti
                                 img_discriminator=img_discriminator)
 
 # set seed
-x, y = next(iter(validataion_set))
-seed = x[:16]
-#seed = tf.random.normal([16, 128, 128, 3])
+#seed = tf.random.normal([16, 32, 32, 128])
+x, _ = next(iter(validataion_set))
+seed_img = x[:8]
+seed_encoded_id = img_id_encoder(seed_img, training=False)
+seed_encoded_exp = img_exp_encoder(seed_img, training=False)
+seed = tf.concat([seed_encoded_id, seed_encoded_exp], axis=0)
 
 @tf.function
 def train_step(dataset):
-    x, y = iter(dataset)
+    x, y = dataset
     image = x
     id_label = y[0]
     va_label = y[1:]
     
-    # with tf.GradientTape() as enc_id_tape, tf.GradientTape() as enc_exp_tape,\
-    #     tf.GradientTape() as cls_id_tape, tf.GradientTape() as cls_exp_tape,\
-    #     tf.GradientTape() as dec_tape, tf.GradientTape() as disc_tape:
     with tf.GradientTape() as cls_id_tape, tf.GradientTape() as cls_exp_tape,\
         tf.GradientTape() as dec_tape, tf.GradientTape() as disc_tape:
         # encoders
@@ -229,38 +229,24 @@ def train_step(dataset):
         fake_output = img_discriminator(decoded_image, training=True)
 
         # losses
-        #enc_id_loss = dispel_loss_fn(id_label, encoded_id_image)
-        #enc_exp_loss = dispel_loss_fn(va_label, encoded_exp_image)
-
         cls_id_loss = cls_id_loss_fn(id_label, decision_id)
-        # TODO : solve Dimensions error
-        #import pdb; pdb.set_trace()
         cls_exp_loss = cls_exp_loss_fn(va_label, decision_exp)
 
         dec_loss = pixel_loss_fn(image, decoded_image)
-
         dis_loss = d_loss_fn(real_output, fake_output)
 
     # gradients
-    #gradients_of_img_id_encoder = enc_id_tape.gradient(enc_id_loss, img_id_encoder.trainable_variables)
-    #gradients_of_img_exp_encoder = enc_exp_tape.gradient(enc_exp_loss, img_exp_encoder.trainable_variables)
-
     gradients_of_img_id_classifier = cls_id_tape.gradient(cls_id_loss, img_id_classifier.trainable_variables)
     gradients_of_img_exp_classifier = cls_exp_tape.gradient(cls_exp_loss, img_exp_classifier.trainable_variables)
 
     gradients_of_img_decoder = dec_tape.gradient(dec_loss, img_decoder.trainable_variables)
-
     gradients_of_img_discriminator = disc_tape.gradient(dis_loss, img_discriminator.trainable_variables)
 
     # optimizers
-    #img_id_encoder_optimizer.apply_gradients(zip(gradients_of_img_id_encoder, img_id_encoder.trainable_variables))
-    #img_exp_encoder_optimizer.apply_gradients(zip(gradients_of_img_exp_encoder, img_exp_encoder.trainable_variables))
-    
     img_id_classifier_optimizer.apply_gradients(zip(gradients_of_img_id_classifier, img_id_classifier.trainable_variables))
     img_exp_classifier_optimizer.apply_gradients(zip(gradients_of_img_exp_classifier, img_exp_classifier.trainable_variables))
 
     img_decoder_optimizer.apply_gradients(zip(gradients_of_img_decoder, img_decoder.trainable_variables))
-
     img_discriminator_optimizer.apply_gradients(zip(gradients_of_img_discriminator, img_discriminator.trainable_variables))
 
 
@@ -268,22 +254,25 @@ def train(dataset, epochs):
     for epoch in tqdm(range(epochs), desc="EPOCHS"):
         start = time.time()
 
+        batches = 0
         for image_batch in tqdm(dataset, desc="BATCHES"):
             train_step(image_batch)
+            batches += 1
+            
+            if batches >= len(dataset):
+                # we need to break the loop by hand because
+                # the generator loops indefinitely
+                break
 
-        # GIF를 위한 이미지를 바로 생성합니다.
-        #display.clear_output(wait=True)
+        # save generated images
         generate_and_save_images(img_decoder, epoch + 1, seed)
 
-        # 15 에포크가 지날 때마다 모델을 저장합니다.
-        if (epoch + 1) % 15 == 0:
-            checkpoint.save(file_prefix = checkpoint_prefix)
+        # save model each EPOCH
+        checkpoint.save(file_prefix = checkpoint_prefix)
 
-        # print (' 에포크 {} 에서 걸린 시간은 {} 초 입니다'.format(epoch +1, time.time()-start))
         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
-    # 마지막 에포크가 끝난 후 생성합니다.
-    #display.clear_output(wait=True)
+    # save generated images (end of epoch)
     generate_and_save_images(img_decoder, epochs, seed)
 
 def generate_and_save_images(model, epoch, test_input):
@@ -296,33 +285,9 @@ def generate_and_save_images(model, epoch, test_input):
         plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
         plt.axis('off')
 
-    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.savefig(os.path.join(OUT_PATH, 'image_at_epoch_{:04d}.png'.format(epoch)))
     plt.show()
 
 # 5. model train
 train(training_set, EPOCHS)
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-
-"""
-# 6. gen gifs
-# 에포크 숫자를 사용하여 하나의 이미지를 보여줍니다.
-def display_image(epoch_no):
-    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
-
-anim_file = 'dcgan.gif'
-
-with imageio.get_writer(anim_file, mode='I') as writer:
-    filenames = glob.glob('image*.png')
-    filenames = sorted(filenames)
-    last = -1
-    for i,filename in enumerate(filenames):
-        frame = 2*(i**0.5)
-        if round(frame) > round(last):
-            last = frame
-        else:
-            continue
-        image = imageio.imread(filename)
-        writer.append_data(image)
-    image = imageio.imread(filename)
-    writer.append_data(image)
-"""
